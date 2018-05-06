8f3bb5b43801dd9c1537b1aa96c8e0959c51a070
Rell's Guest <rellsguest@Durells-MacBook-Pro.local>
2018-05-06 19:28:47 -0400

    All changes will be saved

---
+++ install_jenkins.py
@@ -0,0 +1,16 @@
+"""Jenkins installation:
+
+sudo apt-get update
+sudo apt-get install apache2
+sudo wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins"""
+
+Username: solidsnake
+Password: googleit
+Name:DJ
+Email: Durellhart@live.com
+Jenkins url: http://18.233.160.221:8080
+
+
